1a756612f921ffe3559c84c7c19aa6f8e80d764a
Aurélien <aurelienarnaud921@gmail.com>
2025-01-24 10:00:30 +0700

    supression des espace dans le fichier maugallery.js

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -1,9 +1,7 @@
 (function($) {
   $.fn.mauGallery = function(options) {
-
     var options = $.extend($.fn.mauGallery.defaults, options);
     var tagsCollection = []; // data list des IMG a afficher dans la gallery
-
     return this.each(function() {
       //Cree une Div .gallery-items-row
       $.fn.mauGallery.methods.createRowWrapper($(this));
@@ -43,13 +41,7 @@
       $(this).fadeIn(500);
     });
   };
-
-
-
-
-
   /********** Paramaitre par defaut **********/
-
   $.fn.mauGallery.defaults = {
     columns: 3,
     lightBox: true,
@@ -58,16 +50,8 @@
     tagsPosition: "bottom",
     navigation: true
   };
-
-
-
-
-
-
-
   /********** Listener **********/
   $.fn.mauGallery.listeners = function(options) {
-
     // Listener pour ouvrir la modal
     $(".gallery-item").on("click", function() {
       if (options.lightBox && $(this).prop("tagName") === "IMG") {
@@ -76,10 +60,8 @@
         return;
       }
     });
-
     // Listener pour les filtres
     $(".gallery").on("click", ".nav-link", $.fn.mauGallery.methods.filterByTag);
-
     // Listener pour les boutons de navigation
     $(".gallery").on("click", ".mg-prev", () =>
       $.fn.mauGallery.methods.prevImage(options.lightboxId)
@@ -88,16 +70,8 @@
       $.fn.mauGallery.methods.nextImage(options.lightboxId)
     );
   };
-
-
-
-
-
-
-
   /********** Fonctions **********/
   $.fn.mauGallery.methods = {
-
     /** Cree une Div .gallery-items-row dans element
      * @param {HTMLElement} element
     */
@@ -107,7 +81,6 @@
         element.append('<div class="gallery-items-row row"></div>');
       }
     },
-
     /** Wrap un element dans une Div .item-column
      * @param {HTMLElement} element
      * @param {Object} columns
@@ -148,14 +121,12 @@
         );
       }
     },
-
     /** Ajout element a .gallery-items-row 
      * @param {HTMLElement} element 
     */
     moveItemInRowWrapper(element) {
       element.appendTo(".gallery-items-row");
     },
-
     /** Ajout de la class img-fluid a element
      * @param {HTMLElement} element
     */
@@ -165,7 +136,6 @@
         element.addClass("img-fluid");
       }
     },
-
     /** Affiche ou masque la modal
      * @param {HTMLElement} element
      * @param {String} lightboxId 
@@ -175,7 +145,6 @@
       $(`#${lightboxId}`).find(".lightboxImage").attr("src", element.attr("src"));
       $(`#${lightboxId}`).modal("toggle");
     },
-
     /** Filtre les image et init l'image pre
      * 
     */
@@ -187,7 +156,6 @@
           activeImage = $(this);
         }
       });
-
       let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
       let imagesCollection = [];
       // si le btn filtre all est activer, j'ajoute chaque .item-column au tableaux imagesCollection
@@ -206,7 +174,6 @@
           }
         });
       }
-
       let index = 0,
         next = null;
       // pour chaque image de imagesCollection, si l'image active est egal a l'image de l'index i, je set l'index a i
@@ -215,12 +182,10 @@
           index = i-1; //AJOUT DE -1 POUR CORRIGER LE BUG IMG PRECEDENTE
         }
       });
-
       // set next a l'image suivante de l'index i ou a la derniere image de la collection et j'attribue l'attribut src de next a .lightboxImage
       next = imagesCollection[index] || imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
-
     /** Filtre les image et init l'image suiv
      * 
     */
@@ -232,7 +197,6 @@
           activeImage = $(this);
         }
       });
-
       let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
       let imagesCollection = [];
       // si activeTag egal 'all', j'ajoute chaque .item-column au tableaux imagesCollection
@@ -251,7 +215,6 @@
           }
         });
       }
-
       let index = 0,
         next = null;
       // pour chaque image de imagesCollection, si l'image active est egal a l'image de l'index i, je set l'index a i
@@ -260,12 +223,10 @@
           index = i+1; //AJOUT DE +1 POUR CORRIGER LE BUG IMG 
         }
       });
-
       // set next a l'image suivante de l'index i ou a la premiere image de la collection et j'attribue l'attribut src de next a .lightboxImage
       next = imagesCollection[index] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
-
     /** Cree la modal
      * @param {HTMLElement} gallery
      * @param {String} lightboxId
@@ -293,7 +254,6 @@
         </div>`
       );
     },
-
     /** Creer les bouton de filtre
      * @param {HTMLElement} gallery
      * @param {String} position
@@ -308,7 +268,6 @@
       });
       // isserssion des bouton de filtre dans une ul
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
-
       // Ajout des bouton de filtre en haut ou en bas de la galerie
       if (position === "bottom") {
         gallery.append(tagsRow);
@@ -318,7 +277,6 @@
         console.error(`Unknown tags position: ${position}`);
       }
     },
-
     /** Filtre les image
      * 
     */
@@ -327,22 +285,17 @@
       if ($(this).hasClass("active-tag")) {
         return;
       }
-
       // je retire la class active-tag de l'element actif et je l'ajoute a l'element cliquer
       $(".active-tag").removeClass("active active-tag");
       $(this).addClass("active active-tag"); // AJOUT DE LA CLASSE ACTIVE POUR CORRECTION BUG (fond de couleur dorée)
-
       var tag = $(this).data("images-toggle");
-
       //je cache tout les .item-column
       $(".gallery-item").each(function() {
         $(this).parents(".item-column").hide();
-
         //si le tag est 'all', je montre tout les .item-column,
         if (tag === "all") {
           $(this).parents(".item-column").show(300);
         } 
-
         // autrement, je montre les .item-column qui ont le meme data-gallery-tag que le tag
         else if ($(this).data("gallery-tag") === tag) {
           $(this).parents(".item-column").show(300);
