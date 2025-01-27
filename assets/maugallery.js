c134acdd1d39c902dfbab27c0548b8319bbb28d5
Aurélien <aurelienarnaud921@gmail.com>
2025-01-27 12:48:41 +0700

    feature commentaire

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -41,6 +41,7 @@
       $(this).fadeIn(500);
     });
   };
+
   /********** Paramaitre par defaut **********/
   $.fn.mauGallery.defaults = {
     columns: 3,
@@ -50,6 +51,8 @@
     tagsPosition: "bottom",
     navigation: true
   };
+
+
   /********** Listener **********/
   $.fn.mauGallery.listeners = function(options) {
     // Listener pour ouvrir la modal
@@ -60,8 +63,10 @@
         return;
       }
     });
+
     // Listener pour les filtres
     $(".gallery").on("click", ".nav-link", $.fn.mauGallery.methods.filterByTag);
+
     // Listener pour les boutons de navigation
     $(".gallery").on("click", ".mg-prev", () =>
       $.fn.mauGallery.methods.prevImage(options.lightboxId)
@@ -70,6 +75,8 @@
       $.fn.mauGallery.methods.nextImage(options.lightboxId)
     );
   };
+
+
   /********** Fonctions **********/
   $.fn.mauGallery.methods = {
     /** Cree une Div .gallery-items-row dans element
@@ -81,6 +88,7 @@
         element.append('<div class="gallery-items-row row"></div>');
       }
     },
+
     /** Wrap un element dans une Div .item-column
      * @param {HTMLElement} element
      * @param {Object} columns
@@ -121,12 +129,14 @@
         );
       }
     },
+
     /** Ajout element a .gallery-items-row 
      * @param {HTMLElement} element 
     */
     moveItemInRowWrapper(element) {
       element.appendTo(".gallery-items-row");
     },
+
     /** Ajout de la class img-fluid a element
      * @param {HTMLElement} element
     */
@@ -136,6 +146,7 @@
         element.addClass("img-fluid");
       }
     },
+
     /** Affiche ou masque la modal
      * @param {HTMLElement} element
      * @param {String} lightboxId 
@@ -145,6 +156,7 @@
       $(`#${lightboxId}`).find(".lightboxImage").attr("src", element.attr("src"));
       $(`#${lightboxId}`).modal("toggle");
     },
+
     /** Filtre les image et init l'image pre
      * 
     */
@@ -156,6 +168,7 @@
           activeImage = $(this);
         }
       });
+
       let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
       let imagesCollection = [];
       // si le btn filtre all est activer, j'ajoute chaque .item-column au tableaux imagesCollection
@@ -165,6 +178,7 @@
             imagesCollection.push($(this).children("img"));
           }
         });
+
       } 
       // si non un autre btn filtre est activer, alors si le btn data tag === au img data tag de .item-column, je le rajoute au tableaux imagesCollection 
       else {
@@ -173,7 +187,9 @@
             imagesCollection.push($(this).children("img"));
           }
         });
+
       }
+
       let index = 0,
         next = null;
       // pour chaque image de imagesCollection, si l'image active est egal a l'image de l'index i, je set l'index a i
@@ -182,10 +198,13 @@
           index = i-1; //AJOUT DE -1 POUR CORRIGER LE BUG IMG PRECEDENTE
         }
       });
-      // set next a l'image suivante de l'index i ou a la derniere image de la collection et j'attribue l'attribut src de next a .lightboxImage
+
+      // set next a l'image suivante de l'index i ou a la derniere image de la collection 
+      // et j'attribue l'attribut src de next a .lightboxImage
       next = imagesCollection[index] || imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
+
     /** Filtre les image et init l'image suiv
      * 
     */
@@ -197,6 +216,7 @@
           activeImage = $(this);
         }
       });
+
       let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
       let imagesCollection = [];
       // si activeTag egal 'all', j'ajoute chaque .item-column au tableaux imagesCollection
@@ -206,6 +226,7 @@
             imagesCollection.push($(this).children("img"));
           }
         });
+
       } 
       // autrement, j'ajoute chaque .item-column qui a la meme img data-gallery-tag que activeTag au tableaux imagesCollection
       else {
@@ -215,18 +236,22 @@
           }
         });
       }
+
       let index = 0,
         next = null;
       // pour chaque image de imagesCollection, si l'image active est egal a l'image de l'index i, je set l'index a i
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i+1; //AJOUT DE +1 POUR CORRIGER LE BUG IMG 
+          index = i+1; //AJOUT DE +1 POUR CORRIGER LE PROBLEME D'AFFICHAGE de L'IMAGE SUIVANTE  
         }
       });
-      // set next a l'image suivante de l'index i ou a la premiere image de la collection et j'attribue l'attribut src de next a .lightboxImage
+
+      // set next a l'image suivante de l'index i ou a la premiere image de la collection 
+      // et j'attribue l'attribut src de next a .lightboxImage
       next = imagesCollection[index] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
+
     /** Cree la modal
      * @param {HTMLElement} gallery
      * @param {String} lightboxId
@@ -254,6 +279,7 @@
         </div>`
       );
     },
+
     /** Creer les bouton de filtre
      * @param {HTMLElement} gallery
      * @param {String} position
@@ -266,6 +292,7 @@
       $.each(tags, function(index, value) {
         tagItems += `<li class="nav-item active"><span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
       });
+
       // isserssion des bouton de filtre dans une ul
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
       // Ajout des bouton de filtre en haut ou en bas de la galerie
@@ -277,6 +304,7 @@
         console.error(`Unknown tags position: ${position}`);
       }
     },
+
     /** Filtre les image
      * 
     */
@@ -285,16 +313,20 @@
       if ($(this).hasClass("active-tag")) {
         return;
       }
+
       // je retire la class active-tag de l'element actif et je l'ajoute a l'element cliquer
       $(".active-tag").removeClass("active active-tag");
       $(this).addClass("active active-tag"); // AJOUT DE LA CLASSE ACTIVE POUR CORRECTION BUG (fond de couleur dorée)
       var tag = $(this).data("images-toggle");
+
       //je cache tout les .item-column
       $(".gallery-item").each(function() {
         $(this).parents(".item-column").hide();
+
         //si le tag est 'all', je montre tout les .item-column,
         if (tag === "all") {
           $(this).parents(".item-column").show(300);
+
         } 
         // autrement, je montre les .item-column qui ont le meme data-gallery-tag que le tag
         else if ($(this).data("gallery-tag") === tag) {
